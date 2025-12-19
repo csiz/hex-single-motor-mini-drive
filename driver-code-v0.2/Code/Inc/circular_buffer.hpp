efb0a1da80da784d81fa7706d5116d09be0c1547
Calin <calin.mocanu@gmail.com>
2025-12-19 12:13:50 +0000

    Turn the circular buffer into a class as it's used only from C++.

--- driver-code-v0.2/Code/Inc/circular_buffer.hpp
+++ driver-code-v0.2/Code/Inc/circular_buffer.hpp
@@ -4,130 +4,133 @@
 #include <cstdint>
 
 
-struct CircularBuffer {
+class CircularBuffer {
   uint8_t * buffer;
   size_t max_size;
   volatile size_t write_head = 0;
   volatile size_t read_tail = 0;
   volatile size_t read_head = 0;
+
+public:
+  CircularBuffer(uint8_t * buffer, size_t max_size)
+    : buffer(buffer), max_size(max_size) {}
+
+  void reset() {
+    write_head = 0;
+    read_tail = 0;
+    read_head = 0;
+  }
+
+  uint8_t * reserve_write_head(size_t size) {
+    if (size == 0) return nullptr;
+
+    if (write_head < read_tail) {
+      // We are in wrap around mode.
+
+      // Check if there's anything reserved for reading.
+      if (read_head < read_tail) {
+        // Reading is reserved between read_head and read_tail.
+      
+        // We can only write if we have enough space before the read head.
+        return (write_head + size <= read_head) ? &buffer[write_head] : nullptr;
+      } else {
+
+        // Reading was done, reset the read pointers to what was written in the wrap around.
+        read_tail = write_head;
+        read_head = 0;
+
+        return (write_head + size <= max_size) ? &buffer[write_head] : nullptr;
+      }
+
+
+    } else {
+      // We are after the read area so there is space until the end of the buffer.
+      // `write_head >= read_tail`
+
+      // If reading was done, we can reset the whole buffer ahead of time.
+      if (read_head >= read_tail) reset();
+
+      // We have a choice to write at the end or wrap around to the start.
+      if (write_head + size <= max_size) {
+        // We have enough space at the end of the buffer.
+        return &buffer[write_head];
+
+      } else if (size <= read_head) {
+        // We have enough space at the start of the buffer; but we need to mark the wrap around.
+        write_head = 0;
+        return &buffer[0];
+
+      } else {
+
+        // Not enough space to write the data.
+        return nullptr;
+      }
+    }
+  }
+
+  uint8_t mark_write(size_t size){
+    // Compute tail of the written data.
+    const size_t write_tail = write_head + size;
+  
+    // Check whether we are writing after the read area or in the wrap around area.
+    if (write_head < read_tail) {
+      // We are in the wrap around area.
+  
+      // Ensure we did not overwrite unread data.
+      if (write_tail > read_head) return 1;
+  
+      // At this point `write_tail <= read_head`.
+
+      if (read_head >= read_tail) {
+        // We have finished reading already, reset the read head to the data written so far.
+
+        read_tail = write_tail;
+        read_head = 0;
+      }
+  
+      // Only advance the write head, leave the read tail to be reset by a complete read.
+      write_head = write_tail;
+      return 0;
+    } else {
+      // We are writing after the read area, no risk of overwriting unread data.
+  
+      // Ensure we do not exceed the buffer size.
+      if (write_tail > max_size) return 1;
+  
+      // Mark the data for reading.
+      read_tail = write_tail;
+  
+      // Wrap around the write head if we have reached the end of the buffer.
+      write_head = (write_tail < max_size) ? write_tail : 0;
+      return 0;
+    }
+  }
+  
+  size_t available_to_read() {
+    // Since read tail is reset before the read head we should cap our result to 0 in case.
+    const int readable_size = read_tail - read_head;
+    return (readable_size > 0) ? readable_size : 0;
+  }
+  
+  uint8_t * get_read_head() {
+    return &buffer[read_head];
+  }
+
+  uint8_t mark_read(size_t size) {
+    // Check how much data is available to read.
+    const size_t available_size = available_to_read();
+  
+    // Ensure we are not trying to read more than available.
+    if (size > available_size) return 1;
+  
+    // Advance the read head.
+    read_head += size;
+  
+    return 0;
+  }
 };
 
-static inline void buffer_reset(CircularBuffer * buffer) {
-  buffer->write_head = 0;
-  buffer->read_tail = 0;
-  buffer->read_head = 0;
-}
-
-static inline uint8_t * buffer_reserve_write_head(CircularBuffer * buffer, size_t size) {
-  if (size == 0) return nullptr;
 
 
-  if (buffer->write_head < buffer->read_tail) {
-    // We are in wrap around mode.
-
-    // Check if there's anything reserved for reading.
-    if (buffer->read_head == buffer->read_tail) {
-      // Reading was done, reset the read pointers.
-      buffer->read_tail = buffer->write_head;
-      buffer->read_head = 0;
-
-      return (buffer->write_head + size <= buffer->max_size) ? &buffer->buffer[buffer->write_head] : nullptr;
-    } else {
-    
-      // We can only write if we have enough space before the read head.
-      return (buffer->write_head + size < buffer->read_head) ? &buffer->buffer[buffer->write_head] : nullptr;
-    }
 
 
-  } else {
-    // We are after the read area so there is space until the end of the buffer.
-
-    if (buffer->read_head == buffer->read_tail) {
-      // Reading was done, we can reset the whole buffer ahead of time.
-      buffer_reset(buffer);
-    }
-
-    if (buffer->write_head + size <= buffer->max_size) {
-      // We have enough space at the end of the buffer.
-      return &buffer->buffer[buffer->write_head];
-
-    } else if (size < buffer->read_head) {
-      // We have enough space at the start of the buffer; but we need to mark the wrap around.
-      buffer->write_head = 0;
-      return &buffer->buffer[0];
-
-    } else {
-      // Not enough space to write the data.
-      return nullptr;
-    }
-  }
-}
-
-static inline uint8_t buffer_mark_write(CircularBuffer * buffer, size_t size){
-  // Compute the new write tail after writing.
-  const size_t write_tail = buffer->write_head + size;
-
-  // Check whether we are writing after the read area or in the wrap around area.
-  if (buffer->write_head < buffer->read_tail) {
-    // We are in the wrap around area.
-
-    // Ensure we did not overwrite unread data.
-    if (write_tail > buffer->read_head) return 1;
-
-    if (write_tail == buffer->read_tail) {
-      // We have finished reading already, reset the read head to the data written so far.
-      buffer->read_head = 0;
-      // The read tail is already correct.
-    }
-
-    // Only advance the write head, leave the read tail to be reset by a complete read.
-    buffer->write_head = write_tail;
-    return 0;
-  } else {
-    // We are writing after the read area, no risk of overwriting unread data.
-
-    // Ensure we do not exceed the buffer size.
-    if (write_tail > buffer->max_size) return 1;
-
-    // Mark the data for reading.
-    buffer->read_tail = write_tail;
-
-    // Wrap around the write head if we have reached the end of the buffer.
-    buffer->write_head = (write_tail < buffer->max_size) ? write_tail : 0;
-    return 0;
-  }
-}
-
-static inline size_t buffer_available_to_read(CircularBuffer * buffer) {
-  // Since read tail is reset before the read head we should cap our result to 0 in case.
-  const int readable_size = buffer->read_tail - buffer->read_head;
-  return (readable_size > 0) ? readable_size : 0;
-}
-
-static inline uint8_t * buffer_get_read_head(CircularBuffer * buffer) {
-  return &buffer->buffer[buffer->read_head];
-}
-
-
-static inline uint8_t buffer_mark_read(CircularBuffer * buffer, size_t size) {
-  // Check how much data is available to read.
-  const size_t available_size = buffer_available_to_read(buffer);
-
-  // Ensure we are not trying to read more than available.
-  if (size > available_size) return 1;
-
-  // Advance the read head.
-  buffer->read_head += size;
-
-  // If we finished reading all data and the write head is wrapped around, we can reset the read head too.
-  if (buffer->read_head == buffer->read_tail and buffer->write_head < buffer->read_tail) {
-
-    // Set the new read tail to the current write head.
-    buffer->read_tail = buffer->write_head;
-
-    // Reset the read head to 0 for the next buffer_read call.
-    buffer->read_head = 0;
-  }
-
-  return 0;
-}
--- driver-code-v0.2/Code/Src/usb_com.cpp
+++ driver-code-v0.2/Code/Src/usb_com.cpp
@@ -14,15 +14,9 @@ extern "C" uint8_t UserTxBufferFS[APP_TX_DATA_SIZE];
 
 const int max_usb_packet = 512;
 
-CircularBuffer usb_com_rx_buffer = {
-  .buffer = UserRxBufferFS,
-  .max_size = APP_RX_DATA_SIZE,
-};
+CircularBuffer usb_com_rx_buffer = {UserRxBufferFS,APP_RX_DATA_SIZE};
 
-CircularBuffer usb_com_tx_buffer = {
-  .buffer = UserTxBufferFS,
-  .max_size = APP_TX_DATA_SIZE,
-};
+CircularBuffer usb_com_tx_buffer = {UserTxBufferFS, APP_TX_DATA_SIZE};
 
 
 static_assert(APP_RX_DATA_SIZE == 2 * max_usb_packet, "The USB sizes must be twice the max USB packet size.");
@@ -54,14 +48,14 @@ void usb_received(int rx_size) {
   // Ignore data we weren't prepared to receive.
   if (not receiving_active) return;
 
-  if(buffer_mark_write(&usb_com_rx_buffer, rx_size)) error();
+  if(usb_com_rx_buffer.mark_write(rx_size)) error();
 
   usb_prepare_receive();
 }
 
 void usb_prepare_receive() {
   // Prepare to receive more data.
-  uint8_t * const rx_head = buffer_reserve_write_head(&usb_com_rx_buffer, max_usb_packet);
+  uint8_t * const rx_head = usb_com_rx_buffer.reserve_write_head(max_usb_packet);
 
   if (rx_head != nullptr) {
     // Tell the driver where to write the next incoming packet.
@@ -78,7 +72,7 @@ void usb_sent(int len) {
 
   usb_last_interact = HAL_GetTick();
 
-  if(buffer_mark_read(&usb_com_tx_buffer, len)) error();
+  if(usb_com_tx_buffer.mark_read(len)) error();
 }
 
 void usb_prepare_send() {
@@ -92,10 +86,10 @@ void usb_prepare_send() {
   if (usb_pending_send != 0) error();
   
   // Check if there is data to send.
-  usb_pending_send = buffer_available_to_read(&usb_com_tx_buffer);
+  usb_pending_send = usb_com_tx_buffer.available_to_read();
   if (not usb_pending_send) return;
 
-  uint8_t * const tx_head = buffer_get_read_head(&usb_com_tx_buffer);
+  uint8_t * const tx_head = usb_com_tx_buffer.get_read_head();
   USBD_CDC_SetTxBuffer(&hUsbDeviceFS, tx_head, usb_pending_send);
   USBD_CDC_TransmitPacket(&hUsbDeviceFS);
 }
@@ -103,17 +97,17 @@ void usb_prepare_send() {
 bool usb_update(uint8_t * tx_buffer, int tx_size, BufferFunction process_received_data, void (* onreset)()) {
 
   // Read any data in the reading buffer.
-  const int read_available = buffer_available_to_read(&usb_com_rx_buffer);
+  const int read_available = usb_com_rx_buffer.available_to_read();
   if (read_available > 0) {
-    process_received_data(usb_com_rx_buffer.buffer + usb_com_rx_buffer.read_head, read_available);
-    if(buffer_mark_read(&usb_com_rx_buffer, read_available)) error();
+    process_received_data(usb_com_rx_buffer.get_read_head(), read_available);
+    if(usb_com_rx_buffer.mark_read(read_available)) error();
   }
   
   if (not receiving_active) {
     usb_prepare_receive();
   }
 
-  uint8_t * const tx_head = buffer_reserve_write_head(&usb_com_tx_buffer, tx_size);
+  uint8_t * const tx_head = usb_com_tx_buffer.reserve_write_head(tx_size);
 
   const bool can_send = tx_head != nullptr;
 
@@ -121,7 +115,7 @@ bool usb_update(uint8_t * tx_buffer, int tx_size, BufferFunction process_receive
   if (can_send) {
     // There is enough space to queue the data.
     std::memcpy(tx_head, tx_buffer, tx_size);
-    if(buffer_mark_write(&usb_com_tx_buffer, tx_size)) error();
+    if(usb_com_tx_buffer.mark_write(tx_size)) error();
   }
 
   usb_prepare_send();
@@ -132,8 +126,8 @@ bool usb_update(uint8_t * tx_buffer, int tx_size, BufferFunction process_receive
     usb_deinit();
 
     // Reset the buffers.
-    buffer_reset(&usb_com_rx_buffer);
-    buffer_reset(&usb_com_tx_buffer);
+    usb_com_rx_buffer.reset();
+    usb_com_tx_buffer.reset();
 
     onreset();
 
