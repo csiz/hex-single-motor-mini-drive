fc69eefd57dbe4f29b1b6fcf0c40f8c9e7daabf6
Calin <calin.mocanu@gmail.com>
2026-03-27 17:08:11 +0000

    Simplify main function.

--- master-code/src/main.cpp
+++ master-code/src/main.cpp
@@ -18,23 +18,52 @@ static const char* TAG = "main";
 
 int loop_number = 0;
 
-void main_task(void *arg) {
-  // Register this task with the watchdog timer.
-  esp_task_wdt_add(NULL);
-
-  //Initialize NVS, used for WiFi provisioning and other settings.
+void initialise_nvs_flash() {
   esp_err_t ret = nvs_flash_init();
   if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
       ret = nvs_flash_init();
   }
   ESP_ERROR_CHECK(ret);
+}
+
+void blink_status_led(int64_t current_time_ms) {
+  static int64_t last_blink = 0;
+  static bool status_state = false;
+
+  if (current_time_ms - last_blink > 1000) {
+    status_state = !status_state;
+    uint32_t duty = status_state ? 128 : 32;
+    set_status_led_brightness(duty);
+    last_blink = current_time_ms;
+  }
+}
+
+void receive_message(uint8_t* buffer, size_t size) {
+  ESP_LOGI(TAG, "Received WebSocket message of size %d", size);
+  
+  ws_send_binary(buffer, size);
+}
+
+static inline int64_t get_ms_time() {
+  return esp_timer_get_time() / 1000; // Convert to milliseconds
+}
+
+
+void main_task(void *arg) {
+  // Register this task with the watchdog timer.
+  esp_task_wdt_add(NULL);
+
+  //Initialize NVS, used for WiFi provisioning and other settings.
+  initialise_nvs_flash();
 
   
   // Setup everything
   setup_status_led();
   setup_spi_busses();
   setup_display_and_lvgl();
+
+  update_display(get_ms_time());
 
   start_wifi_provisioning();
   
@@ -44,23 +73,15 @@ void main_task(void *arg) {
     ws_send_binary(buffer, size);
   });
   
-  int64_t last_blink = 0;
-  bool status_state = false;
   
   while (1) {
     // Blink the built-in status LED to show the system is running
-    int64_t current_time = esp_timer_get_time() / 1000; // Convert to milliseconds
+    int64_t current_time_ms = get_ms_time();
 
     // Handle displaya and LVGL tasks.
-    update_display(current_time);
+    update_display(current_time_ms);
     
-    
-    if (current_time - last_blink > 1000) {
-      status_state = !status_state;
-      uint32_t duty = status_state ? 128 : 32;
-      set_status_led_brightness(duty);
-      last_blink = current_time;
-    }
+    blink_status_led(current_time_ms);
 
     // We need to reset the watchdog timer regularly.
     esp_task_wdt_reset();
